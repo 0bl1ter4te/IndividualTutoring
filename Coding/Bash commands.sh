349fad68d5ca1646670e4d4483e521876d847ee1
Jenn <jennyhanft94@gmail.com>
2018-04-21 11:51:20 -0400

    Adding in all lessons

---
+++ Coding/Bash commands.sh
@@ -0,0 +1,18 @@
+# Bash commands
+cd ~/Desktop                                # Change directory to ~/Desktop which is just Desktop
+cd ..                                       # Change director to previous folder
+cd ~                                        # Change directory to root folder
+ls                                          # List files and folders
+open .                                      # Opens the current folder you are in
+open filename.txt                           # Opens text filed caled filename
+touch filename                              # Creates an empty file called whatever your file name is
+mkdir foldername                            # Makes a new folder called wahhtever your folder name is
+rm file                                     # Removes file 
+rm -r foldername                            # Removes folder where -r means recursively
+mv filename_or_folder folder                # Move file or folder
+ls -la                                      # List all files and folders and display permissions
+subl filename_or_folder                     # Opens file in sublime
+sudo _action_                               #Forces an action as the root user where permissions may not be sufficient
+
+#Git commands
+git clone repolink                          # Clones a git repo into your local destination
